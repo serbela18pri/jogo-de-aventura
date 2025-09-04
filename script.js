1013da3c645715865d82f859faf175c567a161d4
serbela18pri <priscilla.serbela@escola.pr.gov.br>
2025-09-04 11:48:04 +0000

    metade aula 6

---
+++ script.js
@@ -0,0 +1,8 @@
+const avanca = document.querySelectorALL ('. btn-proximo');
+avanca.forEach();{//para cada clique ele avanca
+   button.addEventListener (clik,function(){
+const atual = document.querySelector('.ativo')
+  const proximo passo = 'passo- 0'= this. getAttribute(data-proximo') 
+
+    atual.classic.remove('ativo') 
+    document.getElementById(proximoPasso).classList.add('ativo') 
